4b70d3ad39c3ecfe271e3265fe51c3bcb1291919
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-16 16:02:26 +0530

    [vishal] uc4 mobileNumber validation using with MsTest And Exception

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,81 @@
+using ExceptionUserResistrationUc4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFirstName()
+        {
+            //AAA Methodology
+
+            //Arrange
+            string Name = "Ram";
+            string expected = "Valid";
+
+            //Act
+            RegExWithException testing = new RegExWithException(Name);
+            string actual = testing.validFirstName();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestLastName()
+        {
+            //AAA Methodology
+
+            //Arrange
+            string LName = "Rat";
+            string expected = "Valid";
+
+            //Act
+            RegExWithException testing = new RegExWithException(LName);
+            string actual = testing.validFirstName();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod]
+        public void TestMail()
+        {
+            //AAA Methodology
+
+            //Arrange
+            string MailId = "abc.xyz@bl.co.in";
+            string expected = "Valid";
+
+            //Act
+            RegExWithException testing = new RegExWithException(MailId);
+            string actual = testing.validMail(MailId);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestMobileNo()
+        {
+            //AAA Methodology
+
+            //Arrange
+            string Mobile = "91 7038762577";
+            string expected = "Valid";
+
+            //Act
+            RegExWithException testing = new RegExWithException(Mobile);
+            string actual = testing.MobileNo(Mobile);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
