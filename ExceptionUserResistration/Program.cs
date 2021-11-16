c25802b1c65c7cdbaa987be9f6505af09b357e83
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-16 16:20:43 +0530

    [vishal] uc5_Password1 8 smallest char validation using with MsTest And Exception

---
+++ ExceptionUserResistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionUserResistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
