21b6ee406a658de00a04c119a0bb2d4506028760
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-19 16:23:33 +0530

    [vishal] using Custom Exceotion UserRegistration

---
+++ CustomException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U10_LamdaExp_UserResistration
+{
+    public class CustomException:Exception
+    {
+        public ExceptionType type; //   // created a veriable of type   
+        public enum ExceptionType
+        {
+            NUll_Type_Exception,
+            Empty_Type_Exception,  // define some enum values by use the custom exception here
+            Class_Not_Found,
+            CONSTRUCTOR_NOT_FOUND,
+            No_Such_Method,
+            Field_Null,
+            Null_Message,
+            EMPTY_MESSAGE,
+            ArgumentNullException
+
+        }
+
+        // parameterise constructer set the exception type ane message
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
