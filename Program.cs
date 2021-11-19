361b686338b85f66b0e85d412aa8a9bcf670824d
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-19 16:29:21 +0530

    [vishal] main program file lamda UserRegistration

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace U10_LamdaExp_UserResistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Resistration Using Ladmda function");
+            
+        }
+    }
+}
